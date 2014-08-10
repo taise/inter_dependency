772ea8bb21698f1c3df70cb8e53e2d65e951d418
taise <taise515@gmail.com>
2014-08-10 18:14:42 +0900

    Show root node info

--- public/js/inter_dependency.js
+++ public/js/inter_dependency.js
@@ -1,4 +1,4 @@
-var diameter = 960,
+var diameter = 560,
     radius = diameter / 2,
     innerRadius = radius - 60,
     width = diameter + 60;
@@ -12,7 +12,7 @@ var bundle = d3.layout.bundle();
 
 var line = d3.svg.line.radial()
 .interpolate("bundle")
-.tension(0.55)
+.tension(0.4)
 .radius(function(d) { return d.y; })
 .angle(function(d) { return d.x / 180 * Math.PI; });
 
@@ -52,11 +52,22 @@ var linkClass = function(d) {
 
 //var egonet_json = "0.egonet.json";
 //var egonet_json = "3077.egonet.json";
-var egonet_json = "14103.egonet.json";
+//var egonet_json = "14103.egonet.json";
+var egonet_json = "1310.egonet.json";
 d3.json(egonet_json, function(error, classes) {
     var nodes = cluster.nodes(classes),
         links = linkHash(nodes),
         feature = classes.feature;
+
+    var rootInfos = d3.select("#rootInfos");
+
+    rootInfos.append("li").append("strong").text("name:" + classes.name);
+    rootInfos.append("li").append("strong").text("degree:" + nodes.length);
+
+    feature.forEach(function(d){
+        rootInfos.append("li")
+        .text(d);
+    })
 
     svg.selectAll(".link")
     .data(bundle(links))
@@ -93,14 +104,14 @@ d3.json(egonet_json, function(error, classes) {
         svg.selectAll(".v" + current_id)
         .classed("active", true)
 
-        var tooltip = d3.select("#tooltip");
-        tooltip.selectAll("li").remove();
+        var nodeInfos = d3.select("#nodeInfos");
+        nodeInfos.selectAll("li").remove();
 
-        tooltip.append("li").append("strong").text("name:" + d.name);
-        tooltip.append("li").append("strong").text("degree:" + d.degree);
+        nodeInfos.append("li").append("strong").text("name:" + d.name);
+        nodeInfos.append("li").append("strong").text("degree:" + d.degree);
 
         data.feature.forEach(function(d){
-            tooltip.append("li")
+            nodeInfos.append("li")
             .text(d);
         })
     })
