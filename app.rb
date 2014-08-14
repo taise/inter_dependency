d104994dafda219ffa1370897745c589b64baeff
taise <taise515@gmail.com>
2014-08-14 18:16:42 +0900

    Auto compiling coffee scripts

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'coffee-script'
 
 get '/' do
   files = Dir.glob("public/json/*").map do |json|
@@ -7,4 +8,9 @@ get '/' do
   end
   @egonets = files.sort_by {|egonet| egonet.to_i}
   erb :index
+end
+
+get '/coffee/*.js' do
+  filename = params[:splat].first
+  coffee "../public/coffee/#{filename}".to_sym
 end
